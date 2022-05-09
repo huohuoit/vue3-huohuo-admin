891346d1adbd887097da520f0b7faaea209ce635
huohuoit <534794892@qq.com>
2022-05-09 17:01:09 +0800

    feat: .env文件配置

---
+++ types/global.d.ts
@@ -0,0 +1,12 @@
+interface ImportMetaEnv extends ViteEnv {
+  __: unknown;
+}
+
+declare interface ViteEnv {
+  VITE_PORT: number;
+  VITE_PUBLIC_PATH: string;
+  VITE_PROXY_DOMAIN: string;
+  VITE_PROXY_DOMAIN_REAL: string;
+  VITE_ROUTER_HISTORY: string;
+  VITE_LEGACY: boolean;
+}
