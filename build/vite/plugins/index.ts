7125dba7a3f8993fad36614f0f025dd46324a3d4
huohuoit <534794892@qq.com>
2022-05-28 00:02:26 +0800

    feat: 支持jsx/tsx语法

--- build/vite/plugins/index.ts
+++ build/vite/plugins/index.ts
@@ -2,6 +2,7 @@ import { PluginOption } from "vite";
 import vue from "@vitejs/plugin-vue";
 import windiCSS from "vite-plugin-windicss";
 import svgLoader from "vite-svg-loader";
+import vueJsx from "@vitejs/plugin-vue-jsx";
 import DefineOptions from "unplugin-vue-define-options/vite";
 
 // 引入其他需要做额外配置的插件
@@ -10,7 +11,9 @@ import { configLegacyPlugin } from "./legacy";
 export function createVitePlugins(VITE_LEGACY) {
   const vitePlugins: (PluginOption | PluginOption[])[] = [
     // 直接引入的配置
-    vue()
+    vue(),
+    // jsx、tsx语法支持
+    vueJsx()
   ];
 
   // vite-plugin-windicss
